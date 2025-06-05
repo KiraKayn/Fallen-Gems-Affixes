7b7c0cb072debc02cbce33b2b082bd919c632792
Kayne <aron.udvardi@gmail.com>
2025-06-05 23:03:41 +0200

    Updated dependency versions, added optional lionfish api and T.O. tweaks dependency, Implemented new Staffs weapon category and added affixes for it (gems coming soon)

---
+++ src/main/java/net/kayn/fallen_gems_affixes/loot/StaffLootCategory.java
@@ -0,0 +1,21 @@
+package net.kayn.fallen_gems_affixes.loot;
+
+import dev.shadowsoffire.apotheosis.adventure.loot.LootCategory;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ItemStack;
+
+public class StaffLootCategory {
+
+    public static final LootCategory STAFF = LootCategory.register(
+            LootCategory.SWORD,
+            "staffs",
+            StaffLootCategory::CastingItem,
+            new EquipmentSlot[]{EquipmentSlot.MAINHAND}
+    );
+
+    private static boolean CastingItem (ItemStack stack) {
+        return stack.is(ItemTags.create(ResourceLocation.fromNamespaceAndPath("fallen_gems_affixes", "staffs")));
+    }
+}
