284a40bbc0de9ec438acdf0dc723a2e474b383db
Kayne <aron.udvardi@gmail.com>
2025-07-05 23:57:18 +0200

    thing

--- src/main/java/net/kayn/fallen_gems_affixes/FallenGemsAffixes.java
+++ src/main/java/net/kayn/fallen_gems_affixes/FallenGemsAffixes.java
@@ -7,7 +7,6 @@ import net.kayn.fallen_gems_affixes.config.ModConfig;
 import net.kayn.fallen_gems_affixes.event.CelestisynthAttributeHandler;
 import net.kayn.fallen_gems_affixes.event.InitNewCodecs;
 import net.kayn.fallen_gems_affixes.event.MobGearGemInjector;
-import net.kayn.fallen_gems_affixes.event.PermanentEffectHandler;
 import net.kayn.fallen_gems_affixes.init.loot.ModLootModifier;
 import net.kayn.fallen_gems_affixes.loot.CelestialLootCategory;
 import net.kayn.fallen_gems_affixes.loot.StaffLootCategory;
@@ -27,7 +26,7 @@ import org.jetbrains.annotations.NotNull;
 @Mod(FallenGemsAffixes.MOD_ID)
 public class FallenGemsAffixes {
     public static final String MOD_ID = "fallen_gems_affixes";
-    private static final Logger LOGGER = LogManager.getLogger();
+    public static final Logger LOGGER = LogManager.getLogger();
 
     public FallenGemsAffixes(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
@@ -47,7 +46,6 @@ public class FallenGemsAffixes {
         AALootCategories.init();
 
         new MaxHealthDamageHandler();
-        MinecraftForge.EVENT_BUS.register(new PermanentEffectHandler());
         MinecraftForge.EVENT_BUS.register(MobGearGemInjector.class);
         MinecraftForge.EVENT_BUS.register(this);
 
