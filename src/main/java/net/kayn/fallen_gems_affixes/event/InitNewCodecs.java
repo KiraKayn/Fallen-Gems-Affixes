5b39374182d77fb070be67c271177846e775946f
Kayne <aron.udvardi@gmail.com>
2025-07-05 23:57:07 +0200

    thing

--- src/main/java/net/kayn/fallen_gems_affixes/event/InitNewCodecs.java
+++ src/main/java/net/kayn/fallen_gems_affixes/event/InitNewCodecs.java
@@ -3,7 +3,6 @@ package net.kayn.fallen_gems_affixes.event;
 import dev.shadowsoffire.apotheosis.adventure.socket.gem.bonus.GemBonus;
 import net.kayn.fallen_gems_affixes.FallenGemsAffixes;
 import net.kayn.fallen_gems_affixes.adventure.socket.gem.bonus.MultiEffectBonus;
-import net.kayn.fallen_gems_affixes.adventure.socket.gem.bonus.PermanentEffectBonus;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -15,7 +14,6 @@ public class InitNewCodecs {
     public static void init(FMLCommonSetupEvent e) {
         e.enqueueWork(() -> {
             GemBonus.CODEC.register(new ResourceLocation("fallen_gems_affixes", "multi_effect"), MultiEffectBonus.CODEC);
-            GemBonus.CODEC.register(new ResourceLocation("fallen_gems_affixes", "permanent_effect"), PermanentEffectBonus.CODEC);
         });
     }
 }
