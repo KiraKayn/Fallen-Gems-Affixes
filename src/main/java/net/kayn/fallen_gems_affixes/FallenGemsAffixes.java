f24220a91079007f13ad8df7e63e4728a1691ed3
Kayne <aron.udvardi@gmail.com>
2025-07-08 10:28:16 +0200

    Removed unused import

--- src/main/java/net/kayn/fallen_gems_affixes/FallenGemsAffixes.java
+++ src/main/java/net/kayn/fallen_gems_affixes/FallenGemsAffixes.java
@@ -17,7 +17,6 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig.Type;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.ModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
