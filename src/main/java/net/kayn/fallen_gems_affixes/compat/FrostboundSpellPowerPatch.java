5026606cbbc7589f69a53b9a290fdd7042395c57
Kayne <aron.udvardi@gmail.com>
2025-07-15 20:48:49 +0200

    removed unused imports

--- src/main/java/net/kayn/fallen_gems_affixes/compat/FrostboundSpellPowerPatch.java
+++ src/main/java/net/kayn/fallen_gems_affixes/compat/FrostboundSpellPowerPatch.java
@@ -11,7 +11,6 @@ import net.minecraftforge.common.util.Lazy;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModList;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class FrostboundSpellPowerPatch {
--- src/main/java/net/kayn/fallen_gems_affixes/event/InitNewCodecs.java
+++ src/main/java/net/kayn/fallen_gems_affixes/event/InitNewCodecs.java
@@ -2,7 +2,6 @@ package net.kayn.fallen_gems_affixes.event;
 
 import dev.shadowsoffire.apotheosis.adventure.affix.AffixRegistry;
 import dev.shadowsoffire.apotheosis.adventure.socket.gem.bonus.GemBonus;
-import net.kayn.fallen_gems_affixes.FallenGemsAffixes;
 import net.kayn.fallen_gems_affixes.adventure.affix.AdaptiveSpellPowerAffix;
 import net.kayn.fallen_gems_affixes.adventure.affix.SpellEffectAffix;
 import net.kayn.fallen_gems_affixes.adventure.socket.gem.ExtraGemBonusRegistry;
@@ -11,7 +10,6 @@ import net.kayn.fallen_gems_affixes.adventure.socket.gem.bonus.MultiEffectBonus;
 import net.kayn.fallen_gems_affixes.adventure.socket.gem.bonus.PermanentEffectBonus;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 public class InitNewCodecs {
