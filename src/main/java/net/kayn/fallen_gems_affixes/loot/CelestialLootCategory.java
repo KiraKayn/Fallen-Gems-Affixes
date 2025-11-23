3f743a7588bb465cac52153bc678df40abffd007
Kayne <aron.udvardi@gmail.com>
2025-11-23 18:27:25 +0100

    Fixed Celestial Loot Category

--- src/main/java/net/kayn/fallen_gems_affixes/loot/CelestialLootCategory.java
+++ src/main/java/net/kayn/fallen_gems_affixes/loot/CelestialLootCategory.java
@@ -5,28 +5,35 @@ import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ItemStack;
 import org.thecelestialworkshop.celestisynth.api.item.CSWeapon;
 
+import java.util.Set;
+
 public class CelestialLootCategory {
 
+    static final Set<LootCategory> RANGED_SET = Set.of(LootCategory.BOW, LootCategory.CROSSBOW, LootCategory.TRIDENT);
+
     public static final LootCategory CELESTIAL_MELEE = LootCategory.register(
-            LootCategory.SWORD,
+            LootCategory.BOW,
             "celestial_melee",
-            CelestialLootCategory::isCelestialWeapon,
+            CelestialLootCategory::isCelestialMelee,
             new EquipmentSlot[]{EquipmentSlot.MAINHAND}
     );
 
     public static final LootCategory CELESTIAL_RANGED = LootCategory.register(
             LootCategory.BOW,
             "celestial_ranged",
-            CelestialLootCategory::isCelestialWeapon,
+            CelestialLootCategory::isCelestialRanged,
             new EquipmentSlot[]{EquipmentSlot.MAINHAND}
     );
 
-    private static boolean isCelestialWeapon(ItemStack stack) {
-        if (stack.getItem() instanceof CSWeapon) {
-            return true;
-        }
+    private static boolean isCelestialMelee(ItemStack stack) {
+        return stack.getItem() instanceof CSWeapon && !isRanged(stack);
+    }
 
-        String className = stack.getItem().getClass().getName();
-        return className.startsWith("org.thecelestialworkshop.celestisynth.common.item.weapons");
+    private static boolean isCelestialRanged(ItemStack stack) {
+        return stack.getItem() instanceof CSWeapon && isRanged(stack);
+    }
+
+    private static boolean isRanged(ItemStack stack) {
+        return RANGED_SET.stream().anyMatch(c -> c.isValid(stack));
     }
 }
