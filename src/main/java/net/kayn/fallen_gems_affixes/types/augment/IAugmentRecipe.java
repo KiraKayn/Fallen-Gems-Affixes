dafc4c43e28820797dd24b809077276438983c57
Kayne <aron.udvardi@gmail.com>
2025-12-08 14:23:18 +0100

    Fix warnings in IAugmentRecipe

--- src/main/java/net/kayn/fallen_gems_affixes/types/augment/IAugmentRecipe.java
+++ src/main/java/net/kayn/fallen_gems_affixes/types/augment/IAugmentRecipe.java
@@ -1,12 +1,13 @@
 package net.kayn.fallen_gems_affixes.types.augment;
 
-import dev.shadowsoffire.apotheosis.adventure.loot.LootCategory;
 import net.kayn.fallen_gems_affixes.Fallen;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.SmithingRecipe;
+
+import java.util.Objects;
 
 import static net.kayn.fallen_gems_affixes.Fallen.AugmentMisc.AUGMENTS;
 
@@ -15,8 +16,8 @@ public interface IAugmentRecipe extends SmithingRecipe {
      * Get the augment data in the ingredient.
      */
     default ListTag getAugmentData(ItemStack stack) {
-        if (stack.hasTag() && stack.getTag().contains(Fallen.AugmentMisc.AUGMENT_DATA)) {
-            return stack.getTagElement(Fallen.AugmentMisc.AUGMENT_DATA).getList(AUGMENTS, Tag.TAG_COMPOUND);
+        if (stack.hasTag() && Objects.requireNonNull(stack.getTag()).contains(Fallen.AugmentMisc.AUGMENT_DATA)) {
+            return Objects.requireNonNull(stack.getTagElement(Fallen.AugmentMisc.AUGMENT_DATA)).getList(AUGMENTS, Tag.TAG_COMPOUND);
         }
         else {
             return new ListTag();
