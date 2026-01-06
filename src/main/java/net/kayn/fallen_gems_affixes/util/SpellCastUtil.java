d0546d725b78e6242f3e91f5468c5e20090b4864
rtxyd <65117312+rtxyd@users.noreply.github.com>
2026-01-06 21:26:01 +0800

    Fix SpellCaseAffix to newest version of ISS

--- src/main/java/net/kayn/fallen_gems_affixes/util/SpellCastUtil.java
+++ src/main/java/net/kayn/fallen_gems_affixes/util/SpellCastUtil.java
@@ -3,15 +3,14 @@ package net.kayn.fallen_gems_affixes.util;
 import io.redspace.ironsspellbooks.api.entity.IMagicEntity;
 import io.redspace.ironsspellbooks.api.magic.MagicData;
 import io.redspace.ironsspellbooks.api.spells.AbstractSpell;
-import io.redspace.ironsspellbooks.api.spells.CastResult;
 import io.redspace.ironsspellbooks.api.spells.CastSource;
 import io.redspace.ironsspellbooks.api.spells.CastType;
 import io.redspace.ironsspellbooks.capabilities.magic.TargetEntityCastData;
-import io.redspace.ironsspellbooks.network.ClientboundUpdateCastingState;
-import io.redspace.ironsspellbooks.setup.Messages;
-import io.redspace.ironsspellbooks.network.spell.ClientboundOnCastStarted;
-import io.redspace.ironsspellbooks.network.spell.ClientboundOnClientCast;
-import io.redspace.ironsspellbooks.network.spell.ClientboundSyncTargetingData;
+import io.redspace.ironsspellbooks.network.casting.OnCastStartedPacket;
+import io.redspace.ironsspellbooks.network.casting.OnClientCastPacket;
+import io.redspace.ironsspellbooks.network.casting.SyncTargetingDataPacket;
+import io.redspace.ironsspellbooks.network.casting.UpdateCastingStatePacket;
+import io.redspace.ironsspellbooks.setup.PacketDistributor;
 import net.kayn.fallen_gems_affixes.FallenGemsAffixes;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
@@ -63,11 +62,6 @@ public class SpellCastUtil {
 
     private static void castSpellForPlayer(AbstractSpell spell, int spellLevel, ServerPlayer serverPlayer, MagicData magicData) {
 
-        CastResult castResult = spell.canBeCastedBy(spellLevel, CastSource.COMMAND, magicData, serverPlayer);
-        if (castResult.message != null) {
-            Messages.sendToPlayer(new ClientboundSetActionBarTextPacket(castResult.message), serverPlayer);
-        }
-
         if (magicData.isCasting()) {
             FallenGemsAffixes.LOGGER.warn("Attempted to trigger affix-cast while player was already casting");
             return;
@@ -86,13 +80,13 @@ public class SpellCastUtil {
 
         spell.onServerPreCast(serverPlayer.level(), spellLevel, serverPlayer, magicData);
 
-        Messages.sendToPlayer(new ClientboundUpdateCastingState(
+        PacketDistributor.sendToPlayer(serverPlayer, new UpdateCastingStatePacket(
                 spell.getSpellId(), spellLevel, effectiveCastTime, CastSource.COMMAND, "command"
-        ), serverPlayer);
+        ));
 
-        Messages.sendToPlayersTrackingEntity(new ClientboundOnCastStarted(
+        PacketDistributor.sendToPlayer(serverPlayer, new OnCastStartedPacket(
                 serverPlayer.getUUID(), spell.getSpellId(), spellLevel
-        ), serverPlayer, true);
+        ));
 
         if (magicData.getAdditionalCastData() instanceof TargetEntityCastData targetingData) {
             var target = targetingData.getTarget((ServerLevel) serverPlayer.level());
@@ -117,9 +111,9 @@ public class SpellCastUtil {
 
         if (effectiveCastTime == 0) {
             spell.onCast(serverPlayer.level(), spellLevel, serverPlayer, CastSource.COMMAND, magicData);
-            Messages.sendToPlayer(new ClientboundOnClientCast(
+            PacketDistributor.sendToPlayer(serverPlayer, new OnClientCastPacket(
                     spell.getSpellId(), spellLevel, CastSource.COMMAND, magicData.getAdditionalCastData()
-            ), serverPlayer);
+            ));
         }
     }
 
@@ -137,13 +131,13 @@ public class SpellCastUtil {
             playerMagicData.setAdditionalCastData(new TargetEntityCastData(livingTarget));
             if (caster instanceof ServerPlayer serverPlayer) {
                 if (spell.getCastType() != CastType.INSTANT) {
-                    Messages.sendToPlayer(new ClientboundSyncTargetingData(livingTarget, spell), serverPlayer);
+                    PacketDistributor.sendToPlayer(serverPlayer, new SyncTargetingDataPacket(livingTarget, spell));
                 }
             }
         } else if (caster instanceof ServerPlayer serverPlayer) {
-            Messages.sendToPlayer(new ClientboundSetActionBarTextPacket(
+            PacketDistributor.sendToPlayer(serverPlayer, new ClientboundSetActionBarTextPacket(
                     Component.translatable("ui.irons_spellbooks.cast_error_target").withStyle(ChatFormatting.RED)
-            ), serverPlayer);
+            ));
         }
     }
 }
