96e6295db88de91601a56400c240d884497cdaef
rtxyd <65117312+rtxyd@users.noreply.github.com>
2026-01-06 21:03:51 +0800

    Fix SpellCaseAffix to newest version of ISS

--- src/main/java/net/kayn/fallen_gems_affixes/adventure/affix/SpellCastAffix.java
+++ src/main/java/net/kayn/fallen_gems_affixes/adventure/affix/SpellCastAffix.java
@@ -14,6 +14,7 @@ import net.kayn.fallen_gems_affixes.FallenGemsAffixes;
 import net.kayn.fallen_gems_affixes.util.SpellCastUtil;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.util.RandomSource;
 import net.minecraft.util.StringUtil;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
@@ -21,7 +22,6 @@ import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
-import net.minecraft.util.RandomSource;
 
 import javax.annotation.Nullable;
 import java.util.*;
