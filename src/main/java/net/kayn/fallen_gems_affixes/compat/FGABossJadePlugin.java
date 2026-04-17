77f83ff043781072b222a33065d76bf94d81f23b
Kayne <aron.udvardi@gmail.com>
2026-04-17 20:25:18 +0200

    Made it so there can be 3 Affixes in one line

--- src/main/java/net/kayn/fallen_gems_affixes/compat/FGABossJadePlugin.java
+++ src/main/java/net/kayn/fallen_gems_affixes/compat/FGABossJadePlugin.java
@@ -8,6 +8,7 @@ import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.StringTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
@@ -36,21 +37,21 @@ public class FGABossJadePlugin implements IWailaPlugin, IEntityComponentProvider
         ListTag out = new ListTag();
         CompoundTag data = living.getPersistentData();
 
-        // Player-compatible affixes
         if (data.contains(EntityAffixHelper.TAG, Tag.TAG_LIST)) {
             for (Tag t : data.getList(EntityAffixHelper.TAG, Tag.TAG_COMPOUND)) {
-                if (!(t instanceof CompoundTag entry)) continue;
-                String id = entry.getString("affix");
-                if (!id.isEmpty()) out.add(StringTag.valueOf(id));
+                if (t instanceof CompoundTag entry) {
+                    String id = entry.getString("affix");
+                    if (!id.isEmpty()) out.add(StringTag.valueOf(id));
+                }
             }
         }
 
-        // Mob-only affixes
         if (data.contains(MobAffixHelper.TAG, Tag.TAG_LIST)) {
             for (Tag t : data.getList(MobAffixHelper.TAG, Tag.TAG_COMPOUND)) {
-                if (!(t instanceof CompoundTag entry)) continue;
-                String id = entry.getString("affix");
-                if (!id.isEmpty()) out.add(StringTag.valueOf(id));
+                if (t instanceof CompoundTag entry) {
+                    String id = entry.getString("affix");
+                    if (!id.isEmpty()) out.add(StringTag.valueOf(id));
+                }
             }
         }
 
@@ -66,8 +67,30 @@ public class FGABossJadePlugin implements IWailaPlugin, IEntityComponentProvider
         if (list.isEmpty()) return;
 
         tooltip.add(Component.translatable("jade.fga.entity_affixes").withStyle(ChatFormatting.GRAY));
-        for (Tag t : list) {
-            tooltip.add(Component.translatable(t.getAsString()).withStyle(ChatFormatting.GOLD));
+
+        MutableComponent line = Component.empty();
+        int countInLine = 0;
+
+        for (int i = 0; i < list.size(); i++) {
+            String affixKey = list.get(i).getAsString();
+            MutableComponent affixComp = Component.translatable(affixKey).withStyle(ChatFormatting.GOLD);
+
+            line.append(affixComp);
+            countInLine++;
+
+            boolean isLastInTotal = (i == list.size() - 1);
+
+            if (!isLastInTotal) {
+                if (countInLine < 3) {
+                    line.append(Component.literal(", ").withStyle(ChatFormatting.GRAY));
+                }
+            }
+
+            if (countInLine == 3 || isLastInTotal) {
+                tooltip.add(line);
+                line = Component.empty();
+                countInLine = 0;
+            }
         }
     }
 
