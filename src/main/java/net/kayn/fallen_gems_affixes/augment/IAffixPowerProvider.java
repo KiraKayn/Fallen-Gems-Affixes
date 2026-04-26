88c6a5322b0ef362ca4ae536eb297793d4a5ddd0
rtxyd <65117312+rtxyd@users.noreply.github.com>
2026-04-27 04:16:06 +0800

    Interfaces

--- src/main/java/net/kayn/fallen_gems_affixes/augment/IAffixPowerProvider.java
+++ src/main/java/net/kayn/fallen_gems_affixes/augment/IAffixPowerProvider.java
@@ -1,5 +1,26 @@
 package net.kayn.fallen_gems_affixes.augment;
 
-public interface IAffixPowerProvider {
+import dev.shadowsoffire.apotheosis.adventure.affix.Affix;
+import dev.shadowsoffire.apotheosis.adventure.affix.effect.DurableAffix;
+import dev.shadowsoffire.apotheosis.adventure.socket.gem.bonus.GemBonus;
+import dev.shadowsoffire.placebo.reload.DynamicHolder;
+import net.rtxyd.fallen.lib.util.IEither;
+import net.rtxyd.fallen.lib.util.ins_attr.IFilterableIAMProvider;
+import net.rtxyd.fallen.lib.util.ins_attr.InsAttributeModifier;
+
+import java.util.function.Predicate;
+
+public interface IAffixPowerProvider extends IFilterableIAMProvider<IEither<DynamicHolder<? extends Affix>, GemBonus>> {
     float getAffixPower();
+    @Override
+    default InsAttributeModifier getModifierBy(IEither<DynamicHolder<? extends Affix>, GemBonus> a){
+        if (test(a)) {
+            return getModifier();
+        }
+        return InsAttributeModifier.EMPTY;
+    }
+    @Override
+    default boolean test(IEither<DynamicHolder<? extends Affix>, GemBonus> a) {
+        return !(a.getA().get() instanceof DurableAffix);
+    }
 }
--- src/main/java/net/kayn/fallen_gems_affixes/augment/IGemPowerProvider.java
+++ src/main/java/net/kayn/fallen_gems_affixes/augment/IGemPowerProvider.java
@@ -1,5 +1,23 @@
 package net.kayn.fallen_gems_affixes.augment;
 
-public interface IGemPowerProvider {
+import dev.shadowsoffire.apotheosis.adventure.affix.Affix;
+import dev.shadowsoffire.apotheosis.adventure.socket.gem.bonus.GemBonus;
+import dev.shadowsoffire.placebo.reload.DynamicHolder;
+import net.rtxyd.fallen.lib.util.IEither;
+import net.rtxyd.fallen.lib.util.ins_attr.IFilterableIAMProvider;
+import net.rtxyd.fallen.lib.util.ins_attr.InsAttributeModifier;
+
+public interface IGemPowerProvider extends IFilterableIAMProvider<IEither<DynamicHolder<? extends Affix>, GemBonus>> {
     float getGemPower();
+    @Override
+    default InsAttributeModifier getModifierBy(IEither<DynamicHolder<? extends Affix>, GemBonus> a){
+        if (test(a)) {
+            return getModifier();
+        }
+        return InsAttributeModifier.EMPTY;
+    }
+    @Override
+    default boolean test(IEither<DynamicHolder<? extends Affix>, GemBonus> a) {
+        return true;
+    }
 }
